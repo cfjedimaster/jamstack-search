627b625d45eb0d7056781f3a279dcf43f7a2a616
Raymond Camden <raymondcamden@gmail.com>
2022-02-24 12:36:21 -0600

    update algolia

--- demos/algolia/.eleventy.js
+++ demos/algolia/.eleventy.js
@@ -1,3 +1,10 @@
+const indexing = require('algolia-indexing');
+require('dotenv').config();
+const fs = require('fs');
+
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+
 module.exports = function(eleventyConfig) {
 
 	const english = new Intl.DateTimeFormat('en');
@@ -12,5 +19,16 @@ module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy("*.css");
 	eleventyConfig.addWatchTarget("*.css");
+
+	eleventyConfig.on('eleventy.after', async () => {
+
+		const credentials = { appId: ALGOLIA_APP_ID, apiKey: ALGOLIA_API_KEY, indexName: 'jamstack_search' };
+		const records = JSON.parse(fs.readFileSync('_site/algolia.json','utf8'));
+		
+		const settings = { searchableAttributes: ['title', 'content'] };
+		await indexing.fullAtomic(credentials, records, settings);
+		console.log('Done indexing');
+
+    });
 
 };
